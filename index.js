a7473f7dabfa03561419cbb538693bcef9b2736d
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-03-03 11:52:11 +0600

    display static cards

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function fetchData() {
 
 function displayData(datas) {
   const aiTools = datas.data.tools;
-  console.log(aiTools);
+  console.log(aiTools[0]);
   const container = document.getElementById("container");
 
   aiTools.forEach((tool) => {
@@ -16,27 +16,31 @@ function displayData(datas) {
     card.innerHTML = `
     <img
     class="object-cover rounded"
-    src="https://images.unsplash.com/photo-1675557009875-436f71457475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    src="${tool.image}"
     alt="chatgpt"
   />
-
   <div class="mt-4">
     <h2 class="text-2xl font-bold text-slate-700 mb-3">Features</h2>
     <ol class="list-decimal list-inside">
-      <li class="text-slate-500">Natural language processing</li>
-      <li class="text-slate-500">Natural language processing</li>
-      <li class="text-slate-500">Natural language processing</li>
+      <li class="text-slate-500">${tool.features[0]}</li>
+      <li class="text-slate-500">${tool.features[1]}</li>
+      <li class="text-slate-500">${tool.features[2]}</li>
     </ol>
     <hr class="my-6" />
-    <h2 class="text-2xl font-bold mb-4 text-slate-700">ChatGPT</h2>
+    <div class="flex justify-between items-center">
+    <div>
+    <h2 class="text-2xl font-bold mb-4 text-slate-700">${tool.name}</h2>
     <div class="flex gap-2">
       <img
         class="w-5 h-5 object-contain"
         src="./icons/calender.svg"
         alt=""
       />
-      <p>11/11/2023</p>
+      <p>${tool.published_in}</p>
     </div>
+    </div>
+    <button class="bg-red-100 w-10 h-10 rounded-full object-contain text-red-600 hover:scale-95"><i class="fa-solid fa-arrow-right"></i></button>
+  </div>
   </div>
      `;
     container.append(card);
